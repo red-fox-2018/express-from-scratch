c6c10011c266f9ecb9080cb21de9ae8373280b8a
Iswanul Umam <umam@sepulsa.com>
2018-04-20 23:13:23 +0700

    finish routes teacher and student

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const ejs = require('ejs');
 const routes = require('./routes');
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 app.use('/', routes);
-
 app.get('*', (req, res) => {
   res.send('Page unavailable!');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
 
+const fs = require('fs');
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => res.send('I love hacktiv8'));
-router.get('/student', (req, res) => res.send('Student page'));
-router.post('/student', (req, res) => res.send('Student page post'));
-router.get('/teachers', (req, res) => res.send('Teachers page'));
+let studentData = fs.readFileSync('./db/student.json');
+let students = JSON.parse(studentData);
+let teacherData = fs.readFileSync('./db/teacher.json');
+let teachers = JSON.parse(teacherData);
+
+router.get('/', (req, res) => {
+  res.render('index')
+});
+
+router.get('/student', (req, res) => {
+  // res.send(JSON.parse(studentData));
+  res.render('student', { students });
+});
+
+// router.post('/student', (req, res) => {
+//   res.render('student');
+// });
+
+router.get('/teacher', (req, res) => {
+  res.render('teacher', { teachers });
+});
 
 module.exports = router;
